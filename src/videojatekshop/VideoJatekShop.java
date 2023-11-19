0fee8b416c708e6b1cfb3cc5dd0475916f3430b1
tar.gergo <tar.gergo21@gmail.com>
2023-11-19 20:50:04 +0100

    kodolás kezdése

--- src/videojatekshop/VideoJatekShop.java
+++ src/videojatekshop/VideoJatekShop.java
@@ -4,6 +4,8 @@
  */
 package videojatekshop;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author TarGergő(SZF_2022_20
@@ -33,15 +35,13 @@ public class VideoJatekShop extends javax.swing.JFrame {
         Jatek3RBnt = new javax.swing.JRadioButton();
         Jatek4RBnt = new javax.swing.JRadioButton();
         Jatek1RendelCb = new javax.swing.JCheckBox();
-        Jatek2RendelCb = new javax.swing.JCheckBox();
         Jatek3RendelCb = new javax.swing.JCheckBox();
         Jatek4RendelCb = new javax.swing.JCheckBox();
+        Jatek2RendelCb = new javax.swing.JCheckBox();
         RendelesBnt = new javax.swing.JButton();
-        informaciokJP = new javax.swing.JPanel();
-        jatekLeirLbl = new javax.swing.JLabel();
         MegseBtn = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
-        jLabel6 = new javax.swing.JLabel();
+        jatekKepLbl = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         rendeloLbl = new javax.swing.JLabel();
         kiszalitLbl = new javax.swing.JLabel();
@@ -49,29 +49,58 @@ public class VideoJatekShop extends javax.swing.JFrame {
         rendelotxtF = new javax.swing.JTextField();
         kiszalitTxtF = new javax.swing.JTextField();
         telefonSzTxtF = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jatekLeirTxtA = new javax.swing.JTextArea();
+        InformacioLbl = new javax.swing.JLabel();
         BGImg_Lbl = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Video Játék Shop");
         setType(java.awt.Window.Type.UTILITY);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         valasztekJp.setBorder(javax.swing.BorderFactory.createTitledBorder("választékok"));
 
-        Jatek1RBnt.setText("játék1");
+        ValasztekRBG.add(Jatek1RBnt);
+        Jatek1RBnt.setSelected(true);
+        Jatek1RBnt.setText("Baldur's Gate III");
+        Jatek1RBnt.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                Jatek1RBntItemStateChanged(evt);
+            }
+        });
 
-        Jatek2RBnt.setText("játék2");
+        ValasztekRBG.add(Jatek2RBnt);
+        Jatek2RBnt.setText("Monster Hunter Rise");
+        Jatek2RBnt.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                Jatek2RBntItemStateChanged(evt);
+            }
+        });
 
-        Jatek3RBnt.setText("játék3");
+        ValasztekRBG.add(Jatek3RBnt);
+        Jatek3RBnt.setText("Halo Infinite");
+        Jatek3RBnt.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                Jatek3RBntItemStateChanged(evt);
+            }
+        });
 
-        Jatek4RBnt.setText("játék4");
+        ValasztekRBG.add(Jatek4RBnt);
+        Jatek4RBnt.setText("Warhammer III");
+        Jatek4RBnt.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                Jatek4RBntItemStateChanged(evt);
+            }
+        });
 
-        Jatek1RendelCb.setText("jCheckBox1");
+        Jatek1RendelCb.setText("Megrendelés");
 
-        Jatek2RendelCb.setText("jCheckBox2");
+        Jatek3RendelCb.setText("Megrendelés");
 
-        Jatek3RendelCb.setText("jCheckBox3");
+        Jatek4RendelCb.setText("Megrendelés");
 
-        Jatek4RendelCb.setText("jCheckBox4");
+        Jatek2RendelCb.setText("Megrendelés");
 
         javax.swing.GroupLayout valasztekJpLayout = new javax.swing.GroupLayout(valasztekJp);
         valasztekJp.setLayout(valasztekJpLayout);
@@ -79,24 +108,18 @@ public class VideoJatekShop extends javax.swing.JFrame {
             valasztekJpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(valasztekJpLayout.createSequentialGroup()
                 .addGap(35, 35, 35)
+                .addGroup(valasztekJpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(Jatek4RBnt)
+                    .addComponent(Jatek3RBnt)
+                    .addComponent(Jatek2RBnt)
+                    .addComponent(Jatek1RBnt))
+                .addGap(18, 18, 18)
                 .addGroup(valasztekJpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(valasztekJpLayout.createSequentialGroup()
-                        .addComponent(Jatek4RBnt)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(Jatek4RendelCb))
-                    .addGroup(valasztekJpLayout.createSequentialGroup()
-                        .addComponent(Jatek3RBnt)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(Jatek3RendelCb))
-                    .addGroup(valasztekJpLayout.createSequentialGroup()
-                        .addComponent(Jatek2RBnt)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(Jatek2RendelCb))
-                    .addGroup(valasztekJpLayout.createSequentialGroup()
-                        .addComponent(Jatek1RBnt)
-                        .addGap(49, 49, 49)
-                        .addComponent(Jatek1RendelCb)))
-                .addContainerGap(96, Short.MAX_VALUE))
+                    .addComponent(Jatek4RendelCb)
+                    .addComponent(Jatek3RendelCb)
+                    .addComponent(Jatek1RendelCb)
+                    .addComponent(Jatek2RendelCb))
+                .addContainerGap(25, Short.MAX_VALUE))
         );
         valasztekJpLayout.setVerticalGroup(
             valasztekJpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -104,12 +127,13 @@ public class VideoJatekShop extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(valasztekJpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, valasztekJpLayout.createSequentialGroup()
-                        .addGroup(valasztekJpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(Jatek1RBnt)
-                            .addComponent(Jatek1RendelCb))
+                        .addComponent(Jatek1RBnt)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(Jatek2RBnt))
-                    .addComponent(Jatek2RendelCb, javax.swing.GroupLayout.Alignment.TRAILING))
+                    .addGroup(valasztekJpLayout.createSequentialGroup()
+                        .addComponent(Jatek1RendelCb)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(Jatek2RendelCb)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(valasztekJpLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(Jatek3RBnt)
@@ -121,9 +145,9 @@ public class VideoJatekShop extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
-        getContentPane().add(valasztekJp, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 41, -1, -1));
+        getContentPane().add(valasztekJp, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 41, 310, -1));
 
-        RendelesBnt.setBackground(new java.awt.Color(102, 255, 255));
+        RendelesBnt.setBackground(new java.awt.Color(0, 153, 255));
         RendelesBnt.setText("Rendelés");
         RendelesBnt.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -132,55 +156,37 @@ public class VideoJatekShop extends javax.swing.JFrame {
         });
         getContentPane().add(RendelesBnt, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 390, 289, 60));
 
-        informaciokJP.setBorder(javax.swing.BorderFactory.createTitledBorder("információk"));
-
-        jatekLeirLbl.setText("játék leírás");
-
-        javax.swing.GroupLayout informaciokJPLayout = new javax.swing.GroupLayout(informaciokJP);
-        informaciokJP.setLayout(informaciokJPLayout);
-        informaciokJPLayout.setHorizontalGroup(
-            informaciokJPLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(informaciokJPLayout.createSequentialGroup()
-                .addGap(63, 63, 63)
-                .addComponent(jatekLeirLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(101, Short.MAX_VALUE))
-        );
-        informaciokJPLayout.setVerticalGroup(
-            informaciokJPLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(informaciokJPLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jatekLeirLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(15, Short.MAX_VALUE))
-        );
-
-        getContentPane().add(informaciokJP, new org.netbeans.lib.awtextra.AbsoluteConstraints(392, 248, -1, -1));
-
-        MegseBtn.setBackground(new java.awt.Color(255, 0, 0));
+        MegseBtn.setBackground(new java.awt.Color(204, 0, 102));
         MegseBtn.setText("Mégse");
+        MegseBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MegseBtnActionPerformed(evt);
+            }
+        });
         getContentPane().add(MegseBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 390, 295, 60));
 
         jPanel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        jLabel6.setText("Játékokkepek");
+        jatekKepLbl.setIcon(new javax.swing.ImageIcon(getClass().getResource("/kepek/game1.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(94, Short.MAX_VALUE)
-                .addComponent(jLabel6)
-                .addGap(80, 80, 80))
+                .addContainerGap(25, Short.MAX_VALUE)
+                .addComponent(jatekKepLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(64, 64, 64)
-                .addComponent(jLabel6)
-                .addContainerGap(103, Short.MAX_VALUE))
+                .addGap(14, 14, 14)
+                .addComponent(jatekKepLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(392, 41, -1, -1));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(369, 30, 300, 200));
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Kiszálitási adatok"));
 
@@ -231,16 +237,53 @@ public class VideoJatekShop extends javax.swing.JFrame {
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 196, -1, -1));
 
+        jatekLeirTxtA.setColumns(20);
+        jatekLeirTxtA.setRows(5);
+        jatekLeirTxtA.setText("Massive RPG\nKörre osztot stratégiai játék.\nAhol te írhatod a saját történeted,\nés mindennek van következménye!");
+        jScrollPane2.setViewportView(jatekLeirTxtA);
+
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 270, 300, 110));
+
+        InformacioLbl.setLabelFor(jatekLeirTxtA);
+        InformacioLbl.setText("informáciok");
+        getContentPane().add(InformacioLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 250, 70, 20));
+
         BGImg_Lbl.setIcon(new javax.swing.ImageIcon(getClass().getResource("/kepek/BG.PNG"))); // NOI18N
-        getContentPane().add(BGImg_Lbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 670, 470));
+        getContentPane().add(BGImg_Lbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 690, 470));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+
     private void RendelesBntActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RendelesBntActionPerformed
-        // TODO add your handling code here:
+
+
     }//GEN-LAST:event_RendelesBntActionPerformed
+
+    private void MegseBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MegseBtnActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_MegseBtnActionPerformed
+
+    private void Jatek1RBntItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_Jatek1RBntItemStateChanged
+        
+        jatekLeirTxtA.setText("Massive RPG \n Körre osztot stratégiai játék.\nAhol te írhatod a saját történeted,\nés mindennek van következménye!");
+    }//GEN-LAST:event_Jatek1RBntItemStateChanged
+
+    private void Jatek2RBntItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_Jatek2RBntItemStateChanged
+       
+        jatekLeirTxtA.setText("Kreálj egy karaktert és vadász különféle \nNagy bestiákat és szerez belőlük nyers anyagot,\n hogy nagyobb zsákmányra vadász.");
+    }//GEN-LAST:event_Jatek2RBntItemStateChanged
+
+    private void Jatek3RBntItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_Jatek3RBntItemStateChanged
+        jatekKepLbl.setIcon(game3.png);
+        jatekLeirTxtA.setText("Fps,Open world játék \nJátsz egyjátékosba vagy coop-ba \n,harcolj az idegenek ellen \nés mentsd  meg a univerzumot.");
+    }//GEN-LAST:event_Jatek3RBntItemStateChanged
+
+    private void Jatek4RBntItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_Jatek4RBntItemStateChanged
+       
+        jatekLeirTxtA.setText("Turn based és Real time Stratégiai játék,\nVálasz egy fájt a nagy vátozaték közül és \ngyözdle az ellenfeleidet,\n akár foglald el az egész világot!");
+    }//GEN-LAST:event_Jatek4RBntItemStateChanged
 
     /**
      * @param args the command line arguments
@@ -279,6 +322,7 @@ public class VideoJatekShop extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel BGImg_Lbl;
+    private javax.swing.JLabel InformacioLbl;
     private javax.swing.JRadioButton Jatek1RBnt;
     private javax.swing.JCheckBox Jatek1RendelCb;
     private javax.swing.JRadioButton Jatek2RBnt;
@@ -290,11 +334,11 @@ public class VideoJatekShop extends javax.swing.JFrame {
     private javax.swing.JButton MegseBtn;
     private javax.swing.JButton RendelesBnt;
     private javax.swing.ButtonGroup ValasztekRBG;
-    private javax.swing.JPanel informaciokJP;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JLabel jatekLeirLbl;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel jatekKepLbl;
+    private javax.swing.JTextArea jatekLeirTxtA;
     private javax.swing.JLabel kiszalitLbl;
     private javax.swing.JTextField kiszalitTxtF;
     private javax.swing.JLabel rendeloLbl;
@@ -303,4 +347,5 @@ public class VideoJatekShop extends javax.swing.JFrame {
     private javax.swing.JTextField telefonSzTxtF;
     private javax.swing.JPanel valasztekJp;
     // End of variables declaration//GEN-END:variables
+
 }
